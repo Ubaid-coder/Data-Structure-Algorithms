a0a594307fe0f840c8c07d27a524b4f30f532d79
unknown <mu0826960@gmail.com>
2026-06-18 03:41:48 -0700

    Leet code

--- LeetCode.ts
+++ LeetCode.ts
@@ -1,22 +1,22 @@
 // 1929 #LeetCode
 
-function concatenationArray(nums: number[]){
-    
-    let n  = nums.length;
+function concatenationArray(nums: number[]) {
+
+    let n = nums.length;
     let ans: number[] = [];
 
-    for(let i=0; i<nums.length; i++){
+    for (let i = 0; i < nums.length; i++) {
         ans[i] = nums[i];
-        ans[i+n] = nums[i];
+        ans[i + n] = nums[i];
     }
     return ans;
 }
 
 // 1920 #Leetcode
-function buildArray(nums: number[]){
+function buildArray(nums: number[]) {
     let ans: number[] = [];
 
-    for(let i=0; i<nums.length; i++){
+    for (let i = 0; i < nums.length; i++) {
         ans[i] = nums[nums[i]];
     }
 
@@ -24,14 +24,30 @@ function buildArray(nums: number[]){
 }
 
 // 1480 #Leetcode
-function runningSum(nums: number[]){
+function runningSum(nums: number[]) {
     const ans = [];
     let currentSum = 0;
     for (let i = 0; i < nums.length; i++) {
-       
+
         currentSum += nums[i];
         ans[i] = currentSum;
-     
-    }  
+
+    }
     return ans;
 }
+
+// 1365 #Leetcode
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+    let ans = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[i]) count++;
+        }
+        ans[i] = count;
+    }
+    return ans;
+}
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
